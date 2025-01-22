1b22e08f4cff26dd6217ad4011276a16e00661f3
NQDuy <duy13102003@gmail.com>
2025-01-22 21:34:42 +0700

    Introduction

--- PTPMQL/DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ PTPMQL/DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd890fa2d38c8cbd87a5f34d4cbbaab70df3363")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18cae07a46a71dee26dcd2e1e4a622b2c6d8d236")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
